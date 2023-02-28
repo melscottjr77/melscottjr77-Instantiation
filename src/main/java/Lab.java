d2be8a13b3c547d963b36bc9acd3b44eb8e8a467
melscottjr77 <melscottjr77@gmail.com>
2023-02-28 23:57:30 +0000

    Completed Lab Instantiation

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Lab obj = new Lab();
+        return obj;
     }
 }
